c09a0aa8f7c27a84bc7b9333157461198220da87
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-06 22:48:41 +0300

    routing all pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React, {useState} from 'react'
+import {Switch, Route} from 'react-router-dom'
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+
+import {Home} from './pages/Home'
+import {Details} from './pages/Details'
+import {NotFound} from './pages/NotFound'
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
   return (
     <>
-      
+      <Header />
+      <Main>
+          <Switch>
+            <Route exact path="/">
+              <Home countries={countries} setCountries={setCountries}/>
+            </Route>
+            <Route path="/country/:name" component={Details}/>
+            <Route component={NotFound}/>
+          </Switch>
+      </Main>
     </>
   );
 }
