99f5761840f476689034c1166ee94da5abecadb8
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-06 22:43:13 +0300

    controls component

---
+++ src/components/Controls.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+import { Search } from './Search';
+import { CustomSelect } from './CustomSelect';
+
+const options = [
+    { value: 'Africa', label: 'Africa' },
+    { value: 'America', label: 'America' },
+    { value: 'Asia', label: 'Asia' },
+    { value: 'Europe', label: 'Europe' },
+    { value: 'Oceania', label: 'Oceania' },
+];
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    @media (min-width: 767px) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+`;
+
+export const Controls = ({ onSearch }) => {
+    const [search, setSearch] = useState('');
+    const [region, setRegion] = useState('');
+
+    useEffect(() => {
+        const regionName = region?.value || '';
+        onSearch(search, regionName);
+    }, [search, region]);
+
+    return (
+        <Wrapper>
+            <Search search={search} setSearch={setSearch} />
+            <CustomSelect
+                options={options}
+                placeholder="Filter by Region"
+                isClearable
+                isSearchable={false}
+                value={region}
+                onChange={setRegion}
+            />
+        </Wrapper>
+    );
+};
